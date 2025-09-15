3dca3a54b08ca14c99447ff68ce7209f4078bf95
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2025-09-16 00:23:52 +0200

    More tests

--- handtokening/signing/apps.py
+++ handtokening/signing/apps.py
@@ -15,19 +15,23 @@ def try_create_dir(path: Path, mode=0o755):
         logging.error(f"Tried to create path '{path}' but got error: {exc}")
 
 
+def set_up_directories():
+    try_create_dir(config.PIN_COMMS_LOCATION, mode=0o775)
+    try_create_dir(config.PIN_COMMS_LOCATION / "requests", mode=0o775)
+    try_create_dir(config.PIN_COMMS_LOCATION / "responses", mode=0o775)
+
+    # Clearing the requests/responses directory should be handled by the
+    # service runner. E.g., via a startup script or systemd's
+    # RuntimeDirectory= option
+
+    try_create_dir(config.STATE_DIRECTORY / "in")
+    try_create_dir(config.STATE_DIRECTORY / "out")
+    try_create_dir(config.TEST_CERTIFICATE_DIRECTORY)
+
+
 class CertificatesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "handtokening.signing"
 
     def ready(self):
-        try_create_dir(config.PIN_COMMS_LOCATION, mode=0o775)
-        try_create_dir(config.PIN_COMMS_LOCATION / "requests", mode=0o775)
-        try_create_dir(config.PIN_COMMS_LOCATION / "responses", mode=0o775)
-
-        # Clearing the requests/responses directory should be handled by the
-        # service runner. E.g., via a startup script or systemd's
-        # RuntimeDirectory= option
-
-        try_create_dir(config.STATE_DIRECTORY / "in")
-        try_create_dir(config.STATE_DIRECTORY / "out")
-        try_create_dir(config.TEST_CERTIFICATE_DIRECTORY)
+        set_up_directories()
